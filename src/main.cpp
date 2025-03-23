6d3f101e47e9fc0a905e4adb8237da693635f0a4
ErickOlivo <OliauErick@outlook.com>
2025-03-23 17:16:55 -0500

    Add implementation files for HuffmanTree and HuffmanNode, and update to use deleteTree from HuffmanUtils

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 #include "frequency.h"
+#include <unordered_map>
+#include "HuffmanNode.h"
+#include "HuffmanTree.h"
+#include "HuffmanUtils.h" // Para deleteTree
+
+
 
 int main() {
     std::string texto = "abracadabra";
 
-    // Llamada a la función de cálculo de frecuecias
+    // 1. Llamada a la función de cálculo de frecuecias
     std::unordered_map<char, int> freqMap = computeFrequencies(texto);
 
     // Muestra las frecuencias en pantalla
     for (auto& pair : freqMap) {
         std::cout << "Caracter: '" << pair.first << "' - Frecuencia: " << pair.second << std::endl;
     }
+
+    // 2. Construir el árbol de Huffman
+    HuffmanNode* root = buildHuffmanTree(freqMap);
+
+    // 3. Imprimir un mensaje de confirmación
+    if (root != nullptr) {
+        std::cout << "Árbol de Huffman constuisto con éxito. " << "Frecuencia total: " << root->frequency << std::endl;
+    }
+    else {
+        std::cout << "No se pudo construir el árbol (texto vacío o error)." << std::endl;
+    }
+
+    // 4. Liberar la memoria del árbol
+    deleteTree(root);
 
     return 0;
 }
@@ -23,4 +43,5 @@ g++ -I../include main.cpp frequency.cpp -o main
 Desde la raíz del proyecto
 g++ -Iinclude src/main.cpp src/frequency.cpp -o main
 
+g++ -Iinclude src/main.cpp src/frequency.cpp src/HuffmanTree.cpp src/HuffmanUtils.cpp -o main
 */
