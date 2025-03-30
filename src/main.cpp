f9b50cdd4d629d8e2e2c60131e5e3def6614f215
ErickOlivo <OliauErick@outlook.com>
2025-03-29 22:48:11 -0500

    Integrate Huffman code generation and output in main program

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "HuffmanNode.h"
 #include "HuffmanTree.h"
 #include "HuffmanUtils.h" // Para deleteTree
+#include "HuffmanCodes.h"
 
 
 
@@ -23,14 +24,27 @@ int main() {
 
     // 3. Imprimir un mensaje de confirmación
     if (root != nullptr) {
-        std::cout << "Árbol de Huffman constuisto con éxito. " << "Frecuencia total: " << root->frequency << std::endl;
+        std::cout << "Árbol de Huffman construisto con éxito. " << "Frecuencia total: " << root->frequency << std::endl;
     }
     else {
         std::cout << "No se pudo construir el árbol (texto vacío o error)." << std::endl;
     }
 
-    // 4. Liberar la memoria del árbol
+    // 4. Generar códigos
+    std::unordered_map<char, std::string> codes = generateHuffmanCodes(root);
+
+    // 5. Mostrar códigos
+    std::cout << "\nCódigos Huffman generados:\n";
+    for (const auto& pair : codes) {
+        std::cout << "Carácter: '" << pair.first << "' => " << pair.second << '\n';
+    }
+
+    // 6. Liberar la memoria del árbol
     deleteTree(root);
+
+    // Liberar memoria, si usamos punteros crudos
+    // (Implementar una función para borrar recursivamente el árbol)
+    // deleteTree(root);
 
     return 0;
 }
@@ -44,4 +58,7 @@ Desde la raíz del proyecto
 g++ -Iinclude src/main.cpp src/frequency.cpp -o main
 
 g++ -Iinclude src/main.cpp src/frequency.cpp src/HuffmanTree.cpp src/HuffmanUtils.cpp -o main
+
+g++ -Iinclude src/main.cpp src/frequency.cpp src/HuffmanTree.cpp src/HuffmanUtils.cpp src/HuffmanCodes.cpp -o main
+
 */
